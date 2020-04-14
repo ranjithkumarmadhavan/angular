117ca4e128619200bc1c14b3225c387726ccdb91
Ranjith Kumar <ranjithkumarmadhavan@gmail.com>
2020-04-14 22:30:39 +0530

    mod test

--- practise/test/src/app/app.component.ts
+++ practise/test/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestserviceService } from './testservice.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  printText = '';
+  public testList = []
+
+  constructor(private _testserviceService : TestserviceService){
+
+  }
+  ngOnInit(){
+
+    this.testList = this._testserviceService.getTestList();
+
+  }
 }
