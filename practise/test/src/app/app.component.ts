9603aa3f07521d62e847f4dc1091eb7fda830943
Ranjith Kumar <ranjithkumarmadhavan@gmail.com>
2020-04-15 23:19:26 +0530

    todo list app ready

--- practise/test/src/app/app.component.ts
+++ practise/test/src/app/app.component.ts
@@ -8,8 +8,11 @@ import { TestserviceService } from './testservice.service';
 })
 export class AppComponent {
   title = 'test';
-  printText = '';
   public testList = []
+  public toDoList: any[] = [{
+    "task" : "Buy milk",
+    "isDone" : true
+  }]
 
   constructor(private _testserviceService : TestserviceService){
 
@@ -18,5 +21,42 @@ export class AppComponent {
 
     this.testList = this._testserviceService.getTestList();
 
+  }
+
+  sortToDoList(){
+    var isDoneList = this.toDoList.filter(t => t.isDone)
+  }
+
+  AppendInToDoList(newToDoList){
+    var temp = {
+      'task':newToDoList,
+      'isDone' : false
+    }
+    this.toDoList.push(temp);
+
+    this.toDoList.sort((a,b) => {
+      return a.isDone - b.isDone;
+    });
+  }
+
+  removeFromToDoList(value){
+    console.log(value);
+    this.toDoList = this.toDoList.filter( t => t.task !== value.task);
+    this.toDoList.sort((a,b) => {
+      return a.isDone - b.isDone;
+    });
+  }
+
+  updateToDoList(item){
+    this.toDoList = this.toDoList.filter(t => t.task !== item.task)
+    var temp = {
+      'task':item.task,
+      'isDone' : !item.isDone
+    }
+    this.toDoList.push(temp);
+
+    this.toDoList.sort((a,b) => {
+      return a.isDone - b.isDone;
+    });
   }
 }
